525afedde2a55d9a1ecb893fa212e02f5885565e
Mikael Pettersson <mikpelinux@gmail.com>
2024-07-27 10:39:10 +0200

    add printf(), snprintf(), sprintf(), vprintf()

--- Makefile
+++ Makefile
@@ -29,9 +29,9 @@ BUILD=	crt0.o libc.a libm.a
 CRT=	crt0.o
 LIBC=	_assert.o _exit.o _is_console.o _mulovf.o _num2dec.o _putc.o _puts.o _vprintf.o \
 	_write.o abort.o calloc.o errno.o exit.o free.o malloc.o memchr.o memcmp.o memcpy.o \
-	memmove.o memset.o putchar.o puts.o qsort.o realloc.o stdfiles.o strchr.o strcmp.o \
-	strcpy.o strdup.o strlen.o strncat.o strncmp.o strncpy.o strndup.o \
-	strnlen.o strrchr.o strspn.o stubs.o
+	memmove.o memset.o printf.o putchar.o puts.o qsort.o realloc.o snprintf.o sprintf.o \
+	stdfiles.o strchr.o strcmp.o strcpy.o strdup.o strlen.o strncat.o strncmp.o strncpy.o \
+	strndup.o strnlen.o strrchr.o strspn.o stubs.o vprintf.o
 LIBM=
 
 build:	$(BUILD)
---
+++ vprintf.c
@@ -0,0 +1,29 @@
+/* vprintf.c
+   Copyright (C) 2024  Mikael Pettersson <mikpelinux@gmail.com>
+
+   This library is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "xlibio.h"
+
+int vprintf(const char *fmt, va_list ap)
+{
+    struct odev o;
+
+    o.s = NULL;
+    o.n = 1;
+
+    return _vprintf(&o, fmt, ap);
+}
