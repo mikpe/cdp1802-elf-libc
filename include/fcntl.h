abed2fe4184e1155bcfc31c14921e660b31394a7
Mikael Pettersson <mikpelinux@gmail.com>
2024-09-18 14:18:46 +0200

    <fcntl.h>: minimal stub

---
+++ include/fcntl.h
@@ -0,0 +1,25 @@
+/* fcntl.h
+   Copyright (C) 2024  Mikael Pettersson <mikpelinux@gmail.com>
+
+   This library is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this library.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef _FCNTL_H
+#define _FCNTL_H
+
+#define O_ACCMODE	0003
+#define O_RDONLY	00
+#define O_WRONLY	01
+#define O_RDWR		02
+
+#endif /* !_FCNTL_H */
