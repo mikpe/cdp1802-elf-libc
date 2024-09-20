9903c5c1822a1452c7c5868e2fff8dec8469d7f9
Mikael Pettersson <mikpelinux@gmail.com>
2024-09-20 11:49:16 +0200

    add vectors to libm stubs

--- cbrt.c
+++ cbrt.c
@@ -19,7 +19,8 @@
 #include "xlibm.h"
 
 static const struct stub_dd cbrt_stub = {
-    "cbrt", 0xA5, 5, {
+    "cbrt", 0xA5, 6, {
+	{ {0x3f4cd3755d7a2e9a}, {0x3fb8877effffa02e} },
 	{ {0x3fff333333333333}, {0x3ff3fd4493802cf8} },
 	{ {0x4008000000000000}, {0x3ff7137449123ef6} },
 	{ {0x4001d0e560418937}, {0x3ff4e4ed5ce21e38} },
